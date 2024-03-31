994940f30d2bea779cc39dc1694d83e08180ceb3
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-31 16:10:08 +0530

    Update conversations.php

--- Public/Pages/Chat/app/helpers/conversations.php
+++ Public/Pages/Chat/app/helpers/conversations.php
@@ -6,11 +6,15 @@ function getConversation($user_id, $conn)
       Getting all the conversations 
       for current (logged in) user
    **/
-  $sql = "SELECT conversations.*, MAX(chats.created_at) as last_message_time
-  FROM conversations
-  LEFT JOIN chats ON conversations.conversation_id = chats.conversation_id
-  WHERE conversations.user_1 = ? OR conversations.user_2 = ?
-  GROUP BY conversations.conversation_id
+  $sql = "SELECT user_1, user_2, MAX(created_at) as last_message_time
+  FROM (
+      SELECT CASE WHEN from_id = ? THEN to_id ELSE from_id END AS user_1,
+             CASE WHEN to_id = ? THEN from_id ELSE to_id END AS user_2,
+             created_at
+      FROM chats
+      WHERE from_id = ? OR to_id = ?
+  ) AS derived_table
+  GROUP BY user_1, user_2
   ORDER BY last_message_time DESC";
 
   $stmt = $conn->prepare($sql);
