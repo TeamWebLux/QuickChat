cadd721715b9021e1a7bfec8c90d2b56148c51bd
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-31 13:01:42 +0530

    Update globalNotifications.js

--- Public/Chats/globalNotifications.js
+++ Public/Chats/globalNotifications.js
@@ -4,11 +4,12 @@ function checkForNewMessages() {
         .then(data => {
             if (data.newMessages) {
                 console.log(data.newMessages);
-                // Play notification sound only if the user has interacted with the page
-                document.addEventListener('click', function() {
-                    var audio = new Audio('../Public/Chats/notification.wav');
-                    audio.play();
-                })
+                // Attempt to play notification sound automatically when new messages are received.
+                var audio = new Audio('../Public/Chats/notification.wav');
+                audio.play().catch(error => {
+                    console.error('Error playing sound:', error);
+                    // This catch block is here because browsers may block the sound from playing automatically without user interaction.
+                });
             }
         })
         .catch(error => console.error('Error fetching new messages:', error));
