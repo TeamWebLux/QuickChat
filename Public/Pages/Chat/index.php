83d3bcc318e396e6718dba74db336dcc5164a0cc
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-31 12:32:19 +0530

    Update index.php

--- Public/Pages/Chat/index.php
+++ Public/Pages/Chat/index.php
@@ -37,7 +37,7 @@
 		if ($_SESSION['role'] == 'User') {
 			// Fetch online agents in the same page
 			$pagename = $_SESSION['page'];
-			$sql = "SELECT * FROM user WHERE role = 'Agent' AND last_seen(last_seen) = 'Active' AND pagename = ? COLLATE utf8mb4_unicode_ci"; // Adjusted collation
+			$sql = "SELECT * FROM user WHERE role = 'Agent' AND last_seen(last_seen COLLATE utf8mb4_unicode_ci) = 'Active' AND pagename = ?";
 			$stmt = $conn->prepare($sql);
 			$stmt->execute([$pagename]);
 			$agents = $stmt->fetchAll(PDO::FETCH_ASSOC);
