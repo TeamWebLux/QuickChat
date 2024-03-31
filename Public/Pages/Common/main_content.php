2aa4ac329fb79626577f83d836f2589ddb4618ac
Jayesh9819 <dubeyjayesh345@gmail.com>
2024-03-31 19:26:39 +0530

    Update main_content.php

--- Public/Pages/Common/main_content.php
+++ Public/Pages/Common/main_content.php
@@ -2,23 +2,9 @@
    <!--Nav Start-->
    <nav class="nav navbar navbar-expand-xl navbar-light iq-navbar">
       <div class="container-fluid navbar-inner">
-         <a href="index.html" class="navbar-brand">
+         <a href="./Portal" class="navbar-brand">
 
             <!--Logo start-->
-            <div class="logo-main">
-               <div class="logo-normal">
-                  <svg class="text-primary icon-30" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                     <path fill-rule="evenodd" clip-rule="evenodd" d="M7.25333 2H22.0444L29.7244 15.2103L22.0444 28.1333H7.25333L0 15.2103L7.25333 2ZM11.2356 9.32316H18.0622L21.3334 15.2103L18.0622 20.9539H11.2356L8.10669 15.2103L11.2356 9.32316Z" fill="currentColor" />
-                     <path d="M23.751 30L13.2266 15.2103H21.4755L31.9999 30H23.751Z" fill="#3FF0B9" />
-                  </svg>
-               </div>
-               <div class="logo-mini">
-                  <svg class="text-primary icon-30" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                     <path fill-rule="evenodd" clip-rule="evenodd" d="M7.25333 2H22.0444L29.7244 15.2103L22.0444 28.1333H7.25333L0 15.2103L7.25333 2ZM11.2356 9.32316H18.0622L21.3334 15.2103L18.0622 20.9539H11.2356L8.10669 15.2103L11.2356 9.32316Z" fill="currentColor" />
-                     <path d="M23.751 30L13.2266 15.2103H21.4755L31.9999 30H23.751Z" fill="#3FF0B9" />
-                  </svg>
-               </div>
-            </div>
             <!--logo End-->
             <h4 class="logo-title d-block d-xl-none">QuickChat</h4>
          </a>
